f3eb135e960977e1b873885f53c017346bd836ca
Katri <katri.palmroth@brightstraining.com>
2022-10-27 20:58:06 +0300

    harj fork

--- src/fi/academy/Main.java
+++ src/fi/academy/Main.java
@@ -2,6 +2,6 @@ package fi.academy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Pull request is the best!");
+        System.out.println("Pull request is the best! I sure think so!");
     }
 }
